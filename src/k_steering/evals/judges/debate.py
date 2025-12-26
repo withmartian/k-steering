859539f3a326fb9e7c17a046dc7784867fb5550b
shreyansjainn <jshrey8@gmail.com>
2025-12-26 16:05:42 +0530

    Added debate judge

---
+++ src/k_steering/evals/judges/debate.py
@@ -0,0 +1,22 @@
+from typing import Dict
+from k_steering.evals.judges.base import BaseLLMJudge
+from k_steering.utils.constants import DEBATE_DESCRIPTIONS, DEBATE_JUDGE_SYSTEM_PROMPT
+
+class DebateJudge(BaseLLMJudge):
+    
+    def __init__(
+        self, 
+        model_name: str = "gpt-4o-mini"
+    ):
+        """
+        Initialize Debate Judge class
+        
+        Args:
+            model_name: Judge Model Name
+        """
+        super().__init__(model_name)
+        self.task = "debates"
+        self.system_prompt = DEBATE_JUDGE_SYSTEM_PROMPT
+        self.style_descriptions = DEBATE_DESCRIPTIONS
+        
+        
